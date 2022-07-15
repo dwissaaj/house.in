6b03f139c61dc952bd8a1700119670097e62a774
dwisaji <dwisetyoaji@jrdataanlyst.com>
2022-07-15 15:58:57 +0700

    register page

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,11 +1,13 @@
-import { styled,Grid, Button, TextField, Typography } from '@mui/material'
-import { Container,Box, palette, display } from '@mui/system'
-import { useFormik } from 'formik'
+import { Typography } from '@mui/material'
+import { Container} from '@mui/system'
+import { useFormik} from 'formik'
 import React, {useState} from 'react'
-import config from '../Constants'
+
 function Register() {
-  const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
+  const [registerPass,setRegisterPass] = useState(false);
+  const [registerUser,setRegisterUser] = useState(false);
+  const [registerEmail,setRegisterEmail] = useState(false);
   const formik = useFormik({
     initialValues:{
       username:'',
@@ -13,9 +15,35 @@ function Register() {
       password:''
     },
     
-    onSubmit: values => {
-      console.log(values)
-    }
+    onSubmit: async values  => {
+      formik.setSubmitting(true)
+      const req = await fetch(
+        "http://localhost:1337/api/auth/local/register",
+        {
+          method: "POST",
+          headers: {
+          "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
+        }
+        
+      );
+      const res = await req.json();
+      if(res.jwt){
+        formik.resetForm(true);
+        setSuccess(true);      
+      }
+      else if (res.error.message === 'Email or Username are already taken') {
+        setRegisterEmail(true)
+      } else if (res.error.message === 'password must be at least 6 characters'){
+        setRegisterPass(true)
+      }
+      else if (res.error.message === 'username must be at least 3 characters'){
+        setRegisterUser(true)
+      }
+      
+    },
+    
   })
   console.log(formik)
   return (
@@ -23,6 +51,21 @@ function Register() {
         <Container>
 
                 <form onSubmit={formik.handleSubmit}>
+                  {success && (
+                  <div>
+                    <Typography>Registered</Typography>
+                  </div>
+                  )}
+                  {registerPass && (
+                  <Typography>Password have minimum 6 character</Typography>
+                  )}
+                  {registerEmail && (
+                  <Typography>Username or Email Have Been Taken</Typography>
+                  )}
+                  {registerUser && (
+                  <Typography>Minimum 3 Character For Username</Typography>
+                  )}
+  
                   <label htmlFor="username">Username</label>
                   <input 
                   id='username'
